71a53a5301cfba091495f7bcbcbcd0939e6e6996
veccv <veccv@github.com>
2024-02-06 13:29:08 +0100

    Refactored LoginForm validation and added zod and @hookform/resolvers dependencies

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,56 +1,50 @@
-import { Field, Form, Formik } from "formik";
+import {Field, Form, Formik} from "formik";
+import {z} from "zod";
 import {
-  Button,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Input,
+    Button,
+    FormControl,
+    FormErrorMessage,
+    FormLabel,
+    Input,
 } from "@chakra-ui/react";
 
 const LoginForm = () => {
-  function validateName(value: string) {
-    let error;
-    if (!value) {
-      error = "Name is required";
-    } else if (value.toLowerCase() !== "naruto") {
-      error = "Jeez! You're not a fan 😱";
-    }
-    return error;
-  }
-
-  return (
-    <Formik
-      initialValues={{ name: "Sasuke" }}
-      onSubmit={(values, actions) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          actions.setSubmitting(false);
-        }, 1000);
-      }}
-    >
-      {(props) => (
-        <Form>
-          <Field name="name" validate={validateName}>
-            {({ field, form }: { field: any; form: any }) => (
-              <FormControl isInvalid={form.errors.name && form.touched.name}>
-                <FormLabel>First name</FormLabel>
-                <Input {...field} placeholder="name" />
-                <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-              </FormControl>
+    const nameSchema = z.string().min(1, {message: "Name is required"});
+    return (
+        <Formik
+            initialValues={{name: "Sasuke"}}
+            onSubmit={(values, actions) => {
+                setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    actions.setSubmitting(false);
+                }, 1000);
+            }}
+            validationSchema={nameSchema}
+            validateOnChange={true}
+        >
+            {(props) => (
+                <Form>
+                    <Field name="name">
+                        {({field, form}: { field: any; form: any }) => (
+                            <FormControl isInvalid={form.errors.name && form.touched.name}>
+                                <FormLabel>First name</FormLabel>
+                                <Input {...field} placeholder="name"/>
+                                <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                            </FormControl>
+                        )}
+                    </Field>
+                    <Button
+                        mt={4}
+                        colorScheme="teal"
+                        isLoading={props.isSubmitting}
+                        type="submit"
+                    >
+                        Submit
+                    </Button>
+                </Form>
             )}
-          </Field>
-          <Button
-            mt={4}
-            colorScheme="teal"
-            isLoading={props.isSubmitting}
-            type="submit"
-          >
-            Submit
-          </Button>
-        </Form>
-      )}
-    </Formik>
-  );
+        </Formik>
+    );
 };
 
 export default LoginForm;
