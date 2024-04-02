08566380787e6b8b8429e3d2c63a959cc42caa35
veccv <veccv@github.com>
2024-04-02 12:23:08 +0200

    Generated new interfaces for paginated models

--- components/ProductsList.tsx
+++ components/ProductsList.tsx
@@ -1,22 +1,21 @@
 import { CircularProgress, Stack, Text } from "@chakra-ui/react";
-import Product from "@/components/Product";
 import { components } from "@/utils/generated-schema";
 import useSWR from "swr";
 import { useFitContext } from "@/context/FitContext";
+import Product from "@/components/Product";
 
 const ProductsList = () => {
   const { fetcher } = useFitContext();
-  const { data: products, error } = useSWR<components["schemas"]["Product"][]>(
-    "/api/api/v1/product/all",
-    fetcher,
-  );
+  const { data: products, error } = useSWR<
+    components["schemas"]["PageProduct"]
+  >("/api/api/v1/product/all", fetcher);
 
   if (error) return <Text>Failed to load</Text>;
   if (!products) return <CircularProgress isIndeterminate color="green.300" />;
 
   return (
     <Stack w="100%">
-      {products.map((product) => (
+      {products.content.map((product) => (
         <Product key={product.id} product={product} />
       ))}
     </Stack>
